7b55151f0bc4d516e16b7b02db2225e56aba0fd3
johnfernandis1999 <140940560+johnfernandis1999@users.noreply.github.com>
2024-08-12 22:36:14 +0530

    nav text added

--- src/components/navigation/NavMenu.jsx
+++ src/components/navigation/NavMenu.jsx
@@ -7,23 +7,27 @@ function NavMenu() {
         <li>
           <a href="#Home" >
             <img src="./home.svg" alt="home" width={25} />
+            <p className="text-container">Home</p>
             
           </a>
         </li>
         <li>
           <a href="#About">
             <img src="./about.svg" alt="about" width={25} />
+            <p className="text-container">About</p>
           </a>
         </li>
         <li>
           <a href="#Projects">
             <img src="./project.svg" alt="project" width={25} />
           </a>
+          <p className="text-container">Projects</p>
         </li>
         <li>
           <a href="#Contact">
             <img src="./contact.svg" alt="contact" width={25} />
           </a>
+          <p className="text-container">Contact</p>
         </li>
       </ul>
     </div>
